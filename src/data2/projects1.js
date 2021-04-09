0b2860698a54bf47ca65bc382d940863e4148949
Carljohan-hornestam <55090560+Carljohan-hornestam@users.noreply.github.com>
2021-04-09 12:57:41 +0200

    Json-file updates

--- src/data2/projects1.js
+++ src/data2/projects1.js
@@ -3,21 +3,22 @@ import image from "../images/exampleImage.jpg";
 export const projects1 = [
   {
     "name": "Super Galaxy Face Melter 2",
-    "description": "adomnafanmfijuangagniuwngikwujgnwkujgnwjuhnwökiujhniöwkjhnwjkuhnwö-kijuhnböwujihjnwöjkuihnwöhiuwnöiuwjnwsjuikn<wöuihngwöuhjnb<wöiuhbjn<wöjuikhnwöiujn<ölikujbw<nöiuw<nbö<ikujwhw<pöuhbjwnusnv<iwnhk",
+    "description": `Den andra utgåvan av Super Galaxy Face Melter. Den här gången är det ett komplett onlinespel med både server och klient.
+     Servern och klienten kommunicerar via Socket.io.`,
     "githubLink": "https://github.com/BitLord69/SGFM2",
     "imageSrc": image,
     "writtenIn": ["Java", "Javascript", "Vue 3", "Node", "Neo4j",]
   },
   {
     "name": "Marvelissimo",
-    "description": "adomnafanmfijuangagniuwngikwujgnwkujgnwjuhnwökiujhniöwkjhnwjkuhnwö-kijuhnböwujihjnwöjkuihnwöhiuwnöiuwjnwsjuikn<wöuihngwöuhjnb<wöiuhbjn<wöjuikhnwöiujn<ölikujbw<nöiuw<nbö<ikujwhw<pöuhbjwnusnv<iwnhk",
+    "description": "En Android app som är skriven i Kotlin. Appen hämtar karaktärer och serier från Marvels API. Man kan även favoritmarkera och skicka favoriter till andra användare.",
     "githubLink": "https://github.com/BitLord69/Marvelissimo",
     "imageSrc": image,
     "writtenIn": ["Kotlin", "Firebase"]
   },
   {
     "name": "Super Galaxy Face Melter",
-    "description": "adomnafanmfijuangagniuwngikwujgnwkujgnwjuhnwökiujhniöwkjhnwjkuhnwö-kijuhnböwujihjnwöjkuihnwöhiuwnöiuwjnwsjuikn<wöuihngwöuhjnb<wöiuhbjn<wöjuikhnwöiujn<ölikujbw<nöiuw<nbö<ikujwhw<pöuhbjwnusnv<iwnhk",
+    "description": "Den första utgåvan av Super Galaxy Face Melter kortspelet. Det är ett konsolprogram som är skrivet i Java.",
     "githubLink": "https://github.com/nekcoj/SuperGalaxyFaceMelter",
     "imageSrc": image,
     "writtenIn": [
